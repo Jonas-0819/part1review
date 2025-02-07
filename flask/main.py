456e915407dedf11db4bb8fefcb24f24d487ce14
温沐錦 <a1124126@mail.nuk.edu.tw>
2025-02-07 15:46:28 +0000

    1

--- flask/main.py
+++ flask/main.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
-app=Flask(__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<h1>hello world<h1>"
+    return '''<h1>這是python的project</h1>
+            <p>這是在codespace環境開發的</p>
+        '''
